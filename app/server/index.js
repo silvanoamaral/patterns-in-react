c109793ed97aececb4740cf738074a14527bb5d2
silvanoamaral <silvano.amaral9@gmail.com>
2020-05-13 22:16:22 -0300

    Update index.js

--- app/server/index.js
+++ app/server/index.js
@@ -12,7 +12,7 @@ app.use("/api/bovespa", obterBovespa)
 
 if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../build/index.html"))
+    res.sendFile(path.resolve(__dirname, "../../build/index.html"))
   })
 }
 
